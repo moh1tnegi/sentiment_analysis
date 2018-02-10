374f4e1439a24c61151061715cccf5d577abf005
mohitNegi <m0hitNegi>
2018-02-11 00:16:12 +0530

    updated driver

--- driver.py
+++ driver.py
@@ -1,11 +1,11 @@
-import tweepy
-import re
-from textblob import TextBlob
-import authentication as auth
+from authentication import auth
+from getTweets import fetch_tweets
 
 if __name__ == '__main__':
-	auth = auth.auth()
-	if not auth:
-		print("Invalid keys or tokens.\nAuthentication Failed!!")
-	else:
-		print("Username:", auth)
+    api_obj, usrName = auth()
+    if not usrName:
+        print("Invalid keys or tokens.\nAuthentication Failed!!")
+    else:
+        print("Logged in as:", usrName)
+
+    tweets = fetch_tweets(api_obj)
