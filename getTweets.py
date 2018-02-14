e7ca8c618dcf30f92a09cbbf9ce9335066fd1842
mohitNegi <m0hitNegi>
2018-02-14 19:23:00 +0530

    update getTweets

--- getTweets.py
+++ getTweets.py
@@ -1,5 +1,7 @@
 def fetch_tweets(api):
-    search_for = input('Enter a name to search for: ')
+    search_for = input('Enter username to search for: ')
     tweet_counts = int(input('Enter the number of tweets to fetch: '))
 
-    tweets = api.search(search_for, tweet_counts)
+    tweets = api.search(q=search_for, counts=tweet_counts)
+    for i in tweets:
+        print(i)
