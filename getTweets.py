936700fa9b93a74c692ee496ba30cf4895cee207
mohitNegi <mohitnegi165@gmail.com>
2018-02-24 23:26:17 +0530

    Delete getTweets.py

--- getTweets.py
+++
@@ -1,7 +0,0 @@
-def fetch_tweets(api):
-    search_for = input('Enter username to search for: ')
-    tweet_counts = int(input('Enter the number of tweets to fetch: '))
-
-    tweets = api.search(q=search_for, counts=tweet_counts)
-    for i in tweets:
-        print(i)
