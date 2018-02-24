322f549ef54ddc91a3193e754ed978bdf30b7030
mohitNegi <mohitnegi165@gmail.com>
2018-02-24 23:30:08 +0530

    Update authentication.py

--- src/authentication.py
+++ src/authentication.py
@@ -6,11 +6,11 @@ def auth():
         find 'em keys at https://apps.twitter.com while you are logged in with your account
         then click the API
         go-to "Keys and Access Tokens" tab for your keys and stuff."""
-    consumer_key = "UqPjzsEsx5MZIUYgX2YeKYg94"  # API Key
-    consumer_secret = "Fzo8L6h1Z3HGWCrDRpMFAVAE63ZhLLtfJQnV2r5sOaO2HQnx18"  # API Secret
+    consumer_key = "xxxxxxxxxxxxxxxxxxxxxxxxx"  # API Key
+    consumer_secret = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"  # API Secret
     # copy-paste the whole key wheather its like:- xxxxxxxxx-xxxxxxxxxxxxxxx
-    access_token = "763711848747241472-gWBtA8OJ6klrHtVlEoiBtlekE6BFlDb"
-    access_token_secret = "raDruNwLV7rcj2pobmXWMJ9fVQevWJIfpCq5q1MqrSkLM"
+    access_token = "xxxxxxxxxxxxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    access_token_secret = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 
     try:
         # initialize the Oauth object for OAuthHandler class in tweepy module
